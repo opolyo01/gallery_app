0be030132a2d05f40a7a6e8e29ee74284945bd43
Oleg Polyakov <opolyo01@yahoo.com>
2025-04-16 12:23:14 -0500

    code changes

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://backend-cx1yspyh0-olegs-projects-f3a8f0e9.vercel.app', // Local backend URL
+  apiUrl: 'https://backend-5g4uhbaic-olegs-projects-f3a8f0e9.vercel.app', // Local backend URL
 };
