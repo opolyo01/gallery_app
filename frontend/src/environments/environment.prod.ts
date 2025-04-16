12f273c2cfe5f22fae5d5131fad0b638284fc975
Oleg Polyakov <opolyo01@yahoo.com>
2025-04-16 13:05:58 -0500

    update urls

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-cx1yspyh0-olegs-projects-f3a8f0e9.vercel.app', // Render backend URL
+  apiUrl: 'https://backend-black-three-28.vercel.app', // Render backend URL
 };
